13243d9ebd17116612f4f551bfc3155f9e9c179d
kevin-zhao-fly <8zhaokai@sina.com>
2018-07-22 23:34:21 +0800

    循环数组

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -11,9 +11,46 @@ export class NewsComponent implements OnInit {
 
   msg:any;
 
+  public obj={
+    name:'张三'
+  }
+
   public id='123';
 
-  constructor() { }
+  public list=[];
+  public list2=[];
+  public list3=[];
+
+  constructor() { 
+    this.list=['111','222','333'];
+    this.list2=[
+      {title:1111111111},
+      {title:2222222222},
+      {title:3333333333}
+    ];
+    this.list3=[
+      {
+        'carname':'马自达',
+        'list':[
+          {'title':'马1'},
+          {'title':'马2'},
+
+          {'title':'马3'},
+
+        ]
+      },
+      {
+        'carname':'宝马',
+        'list':[
+          {'title':'BMW1'},
+          {'title':'BMW2'},
+
+          {'title':'BMW3'},
+
+        ]
+      }
+    ]
+  }
 
   ngOnInit() {
     this.msg='<h2>这是一个msg消息<h2>';
