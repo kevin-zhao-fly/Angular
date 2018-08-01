a9eb13fb343dfafab12e5c53b29243544b5b382d
kevin-zhao-fly <8zhaokai@sina.com>
2018-08-02 00:42:18 +0800

    实现todolist

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodolistComponent implements OnInit {
 
-  constructor() { }
+  public username:any;
+
+  public list;
+  public list1;
+
+  constructor() { 
+    this.username='test';
+    this.list=[];
+    this.list1=[];
+  }
 
   ngOnInit() {
+  }
+
+  getData(){
+   // alert(this.username);
+   this.list.push(this.username);
+  }
+
+  deleteData(key){
+    //alert(key);
+    this.list.splice(key,1);
+  }
+
+  addData(key){
+    // console.log(key);
+    if (key.keyCode==13) {
+      this.list.push(this.username);
+    }
+  }
+
+  moveData(i,key){
+    this.list1.push(key);
+    this.list.splice(i,1);
   }
 
 }
